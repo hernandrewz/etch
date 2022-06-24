f0273703a65890c513e7c73d2f8ec06fb41bf8c2
andrew <andrewh23@protonmail.com>
2022-06-24 09:53:32 -0700

    have function make grid via loop and set all-selector to grab elements and separate them with foreach to apply an event listener that will change bg color of each cell

--- myscript.js
+++ myscript.js
@@ -1,21 +1,75 @@
 const square = document.createElement("div");
 
-square.setAttribute("id", "container")
+square.setAttribute("id", "container");
+
+
+
+    
+
 
 document.body.appendChild(square);
 
 const canvas = document.getElementById("container");
 
+
+
+
 function makeRows(rows, cols,) {
     canvas.style.setProperty('--grid-rows', rows);
     canvas.style.setProperty('--grid-cols', cols);
-    for ( c = 0; c < (rows * cols); c++) {
+    for ( let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
+        
         cell.innerText = (c + 1); 
         container.appendChild(cell).className = "grid-item";
-    }
+        cell.setAttribute("id", "cellS");
+        const cellAdds = document.querySelectorAll(".grid-item");
+        
+        cellAdds.forEach (cellAdd => {
+            cellAdd.addEventListener("mouseover", function () {
+                cellAdd.style.backgroundColor = "blue";
+            });
+            })
+        
+        cellAdds.forEach (cellAdd => {
+            cellAdd.addEventListener("mouseout", function () {
+                cellAdd.style.backgroundColor = "white";
+            });
+            })
+        }
+            
+        }
+    
+
+        
+        
+        
+        
+    
+       
 
 
-}
+       
+     
+        
+    
+    
+    
 
 makeRows(16, 16);
+
+function mouseOver(e) {
+    const bgColor = 'blue';
+   e.target.style.backgroundColor = bgColor;
+  
+   console.log(this)
+   }
+
+
+function mouseOut(e){ 
+    var elements = document.getElementsByClassName("grid-item"); 
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "black";}}
+
+
+        
