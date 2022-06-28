0cdaf258ceccd106996e658784497f969165c903
andrew <andrewh23@protonmail.com>
2022-06-28 07:50:05 -0700

    remove cell inner text

--- myscript.js
+++ myscript.js
@@ -30,9 +30,10 @@ function randomBgColor() {
 function makeRows(rows, cols,) {
     canvas.style.setProperty('--grid-rows', rows);
     canvas.style.setProperty('--grid-cols', cols);
+    
     for ( let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1); 
+        //cell.innerText = (c + 1); 
         container.appendChild(cell).className = "grid-item";
         cell.setAttribute("id", "cellS");
         
